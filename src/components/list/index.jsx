251be4f7016202719ac36003f22f4d2345d070ae
Lucho Web <webfacilco@gmail.com>
2022-08-03 12:06:52 -0500

    next pagination bug fixed

--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -29,7 +29,7 @@ const List = () => {
             </Link>
           </li>
           <li className="page-item">
-            <Link className="page-link" to={`/page/${page >= 1 ? parseInt(page) + 1 : 0}`}>
+            <Link className="page-link" to={`/page/${page > 1 ? parseInt(page) + 1 : 1}`}>
               Next
             </Link>
           </li>
