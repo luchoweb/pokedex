522262622738d929e42435fd9e548114bfa6f081
Lucho Web <webfacilco@gmail.com>
2022-08-03 07:46:25 -0500

    Not found page

--- src/views/404/index.jsx
+++ src/views/404/index.jsx
@@ -1,11 +1,19 @@
 import Layout from "../layout/layout";
 
 import "./styles.scss";
+import Pokeball from "../../assets/images/pokeball.png";
+import { Link } from "react-router-dom";
 
 const NotFoundView = () => {
   return (
     <Layout>
-      <h1>404</h1>
+      <div className="container">
+        <div className="pt-5 pb-5 text-center page-404">
+          <img src={Pokeball} alt="Pokeball" height={100} />
+          <h1 className="display-6 mt-3">Page not found</h1>
+          <p>The page you are trying to visit has been moved or deleted, go to the <Link to="/" className="link">home page</Link>.</p>
+        </div>
+      </div>
     </Layout>
   )
 }
