7ab360807f2ea7690f1a10a0a73542cb469b164a
Lucho Web <webfacilco@gmail.com>
2022-08-03 09:02:07 -0500

    header reponsive

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -24,8 +24,8 @@ const Header = () => {
     <header className="pt-2 pb-2">
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-12 col-md-6">
-            <Link to="/">
+          <div className="col-12 mb-3 mb-md-0 col-md-6">
+            <Link to="/" className="d-block text-center text-md-start">
               <img src={Logo} alt="Pokedex" height={70} width={194} />
             </Link>
           </div>
--- src/components/common/header/menu.jsx
+++ src/components/common/header/menu.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Menu = ({ items }) => {
   return (
-    <ul className="menu list-unstyled m-0 p-0 d-flex align-items-center justify-content-end gap-4">
+    <ul className="menu list-unstyled m-0 p-0 d-flex align-items-center justify-content-center justify-content-md-end gap-4">
       {
         items.map((item, index) => (
           <li className="menu-item" key={`i-${index}`}>
