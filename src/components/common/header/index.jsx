b9de9e9d80cb867a06b78171ce2373f383605cc2
Lucho Web <webfacilco@gmail.com>
2022-08-03 07:27:59 -0500

    header visual done

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -5,12 +5,22 @@ const Header = () => {
   return (
     <header className="pt-2 pb-2">
       <div className="container">
-        <div className="row">
+        <div className="row align-items-center">
           <div className="col-12 col-md-6">
             <img src={Logo} alt="Pokedex" height={70} width={194} />
           </div>
           <div className="col-12 col-md-6">
-            Menu
+            <ul className="menu list-unstyled m-0 p-0 d-flex align-items-center justify-content-end gap-4">
+              <li className="menu-item">
+                <a href="/" className="menu-link">Home</a>
+              </li>
+              <li className="menu-item">
+                <a href="/" className="menu-link">About</a>
+              </li>
+              <li className="menu-item">
+                <a href="/" className="menu-link">Legal</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
