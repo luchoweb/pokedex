7a612add6420f2271beabc5c8a97765a3b2143a1
Luis Rodriguez <luis@deepspacerobots.com>
2022-08-04 11:01:30 -0500

    header padding changed

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -6,10 +6,10 @@ import Menu from "./menu";
 
 const Header = () => {
   return (
-    <header className="pt-2 pb-2">
+    <header className="pt-4 pb-4 pt-md-2 pb-md-2">
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-12 mb-3 mb-md-0 col-md-6">
+          <div className="col-12 mb-4 mb-md-0 col-md-6">
             <Link to="/" className="d-block text-center text-md-start">
               <img src={Logo} alt="Pokedex" height={70} width={194} />
             </Link>
